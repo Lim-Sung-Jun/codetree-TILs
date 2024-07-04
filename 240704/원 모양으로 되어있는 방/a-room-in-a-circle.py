290b7fee4a55feea8790b6b7fddf8eb6f3c2b442
sungjun <61612775+Lim-Sung-Jun@users.noreply.github.com>
2024-07-04 16:20:07 +0900

    Title: 원 모양으로 되어있는 방, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,29 @@
+# # 거리의 최소합? 이해 x
+
+# 어떤 방에서 시작?
+# 방에 인원이 들어가는데 거리의 합?
+# 2번방 시작
+# 거리: 
+
+n = int(input())
+room = []
+
+for i in range(n):
+    room.append(int(input()))
+
+answer = float('inf')
+for i in range(n):
+    # visited = [False] * n
+    distance = 1
+    result = 0
+    j = i + 1
+    # count = 1 # 이걸 딱 4번만 반목하는 방법? 그냥 for문 쓰면 되는구나 n번 반복이네
+    # while count < 5:
+    for k in range(n-1):
+        result += room[j % 5] * distance
+        distance += 1
+        # count += 1
+        j += 1
+    answer = min(result, answer)
+
+print(answer)
