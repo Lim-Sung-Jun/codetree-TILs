3be6e53d87ecf04be5e82482ac35560d0016ece6
sungjun <61612775+Lim-Sung-Jun@users.noreply.github.com>
2024-07-04 16:21:16 +0900

    Title: 원 모양으로 되어있는 방, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240704/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240704/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -3,7 +3,7 @@
 # 어떤 방에서 시작?
 # 방에 인원이 들어가는데 거리의 합?
 # 2번방 시작
-# 거리: 
+# 예시로 풀었으면 -> n으로 일반화해야지
 
 n = int(input())
 room = []
@@ -20,7 +20,7 @@ for i in range(n):
     # count = 1 # 이걸 딱 4번만 반목하는 방법? 그냥 for문 쓰면 되는구나 n번 반복이네
     # while count < 5:
     for k in range(n-1):
-        result += room[j % 5] * distance
+        result += room[j % n] * distance # 이것도 일반화
         distance += 1
         # count += 1
         j += 1
