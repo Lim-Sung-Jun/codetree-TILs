2ad29f03d63f53c40d983377ce08f1f14f94fa69
sungjun <61612775+Lim-Sung-Jun@users.noreply.github.com>
2024-07-15 18:02:11 +0900

    Title: 오목, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/오목/O-mok.py
@@ -0,0 +1,62 @@
+# 인덱스 1시작
+# 검1/흰2/승부x 0
+# 
+
+graph = []
+for i in range(19):
+    graph.append(list(map(int, input().split())))
+
+# 오른 (x, y + 1)
+# 아래 (x + 1, y)
+# 오른 아래 대각선 (x + 1, y + 1)
+# 5번 이동하고 첫점과 마지막점 검사하기
+
+flag = True
+answer = []
+steps = [(0, 1), (1, 0), (1, 1)]
+for i in range(19):
+    for j in range(19):
+        if graph[i][j] != 0:
+            for step in steps:
+                x, y = i, j
+                count = 1
+                flag = True
+                while count < 5:
+                    n_x = x + step[0]
+                    n_y = y + step[1]
+                    if 0 <= n_x <= 18 and 0 <= n_y <= 18 and graph[n_x][n_y] == graph[i][j]:
+                        x = n_x
+                        y = n_y
+                    else:
+                        flag = False
+                        break
+
+                    count += 1
+
+                # 첫 점 끝 점
+                if 0 <= i - 1 <= 18 and 0 <= j - 1 <= 18:
+                    if graph[i - step[0]][j - step[1]] == graph[i][j]:
+                        #print('previous:',i - step[0], j - step[1])
+                        flag = False
+
+                if 0 <= x + 1 <= 18 and 0 <= y + 1 <= 18:        
+                    if graph[x + step[0]][y + step[1]] == graph[i][j]:
+                        #print('next:',x + step[0], y + step[1])
+                        flag = False
+
+                if flag == True:
+                    answer.append(graph[i][j])
+                    answer.append((i + x)/2)
+                    answer.append((j + y)/2)
+                    #print(answer)
+                    break
+            if len(answer) != 0:
+                break
+    if len(answer) != 0:   
+        break
+
+if len(answer) != 0:
+    print(answer[0])
+    print(int(answer[1] + 1), int(answer[2] + 1))
+else:
+    print(0)
