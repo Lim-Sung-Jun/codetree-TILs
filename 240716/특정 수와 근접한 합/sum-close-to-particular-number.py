5f4cd63a440b6f6d4f57499dd39ba2ed71c41d64
sungjun <61612775+Lim-Sung-Jun@users.noreply.github.com>
2024-07-16 11:17:11 +0900

    Title: 특정 수와 근접한 합, Time: 177ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240716/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -3,6 +3,17 @@ n, s = map(int, input().split())
 # abs(합 - s)의 min값 찾기
 n_list = list(map(int, input().split()))
 
+# 너무 쉬운 문제였다.
+# 해설지에서는 위치를 하나씩 잡아봄
+# # # 모든 쌍을 다 잡아봅니다.
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         # i번과 j번 수를 제외할 경우 남은 숫자들의 총합은 다음과 같습니다.
+#         new_sum = array_sum - arr[i] - arr[j]
+# 일차원일 때 두개를 고르는 모든 쌍은 for문 2개로 구현
+# 전체 합에서 두 개 원소 값을 빼주면 된다.
+# 와우,,
+
 from itertools import combinations
 
 all_cases = combinations(n_list, len(n_list) - 2)
