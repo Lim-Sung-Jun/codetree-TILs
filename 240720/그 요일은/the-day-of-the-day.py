e78b650bd9b89b2c7495aaf0bb780824e4f79950
sungjun <61612775+Lim-Sung-Jun@users.noreply.github.com>
2024-07-20 21:06:08 +0900

    Title: 그 요일은, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240720/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,28 @@
+days_per_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+m1, d1, m2, d2 = map(int, input().split())
+key = input()
+
+for i in range(7):
+    if days[i] == key:
+        key = i
+        break
+
+first, second = 0, 0
+for i in range(1, m1):
+    first += days_per_month[i]
+first += d1
+
+for i in range(1, m2):
+    second += days_per_month[i]
+second += d2
+
+result = abs(second - first)
+
+x = result // 7
+y = result % 7
+
+if y <= key:
+    x = x + 1
+
+print(x)
