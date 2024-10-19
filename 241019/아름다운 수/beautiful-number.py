e5bdcbf811d46a8d7ea63a13e2e8d79e089c9c1b
sungjun <61612775+Lim-Sung-Jun@users.noreply.github.com>
2024-10-19 13:27:50 +0900

    Title: 아름다운 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241019/아름다운 수/beautiful-number.py
@@ -0,0 +1,32 @@
+# 1, 2, 3, 4
+# 
+
+n = int(input())
+
+# n -> n자리 모든 수 1,2,3,4를 넣으면서 생기는 케이스
+def beauty(n_list):
+    i = 0
+    while True:
+        if i == len(n_list):
+            break
+        element = n_list[i]
+        for j in range(element):
+            i = i + j
+            if n_list[i] != element:
+                return False
+        i = i + 1
+    return True
+
+def fn(n_list, count):
+    # global n
+    if n == len(n_list):
+        if beauty(n_list):
+            return 1
+
+    for i in range(1, n + 1):
+        n_list.append(i)
+        count += fn(n_list, count)
+    
+    return count
+
+print(fn([], 0))
